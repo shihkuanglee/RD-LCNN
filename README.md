c2da536c2b06884615f47fce4c8769cea99eb760
Shih Kuang Lee <shihkuangl@gmail.com>
2022-05-20 01:36:10 +0800

    update license.

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ pip install -r requirements.txt
 python main.py --cuda_vd 0 --epochs 200 --esep 100 --conifg_section Ceps --lr -1 --path_data ../ASVspoof2019 --task PA
 ```
 
+### Licensing
+
+This repository is licensed under the [ISC License](https://github.com/shihkuanglee/RD-LCNN/blob/main/LICENSE.md).
+
+This repository includes modified code from [Librosa](https://github.com/librosa/librosa), also ISC licensed.
--- dataset.py
+++ dataset.py
@@ -148,7 +148,21 @@ def getitem_ceps(self, idx):
 
 
 def stdct(y, n_fft, hop_length, win_length, window):
-    """Modify from stft implementation of Librosa."""
+    """Modify from stft implementation of Librosa.
+
+    Copyright (c) 2013--2017, librosa development team.
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted, provided that the above
+    copyright notice and this permission notice appear in all copies.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE."""
 
     # By default, use the entire frame
     if win_length is None: win_length = n_fft
